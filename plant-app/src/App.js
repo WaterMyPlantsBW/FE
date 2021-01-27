9765a1e52fc86a6af7cf3a1346c14029a9af122d
Durrell Jules <dujules23@gmail.com>
2021-01-26 23:08:44 -0500

    created onChange function

--- plant-app/src/App.js
+++ plant-app/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+import Login from './Login'
 function App() {
 	return (
 		<div className="App">
 			<h1>Hello Plants!</h1>
+			<Login/>
 		</div>
 	);
 }
--- plant-app/src/Login.js
+++ plant-app/src/Login.js
@@ -4,40 +4,63 @@ import styled from 'styled-components'
 
 
 export default function Login (){
+
+    const [login, setLogin] = useState({
+        username: '',
+        password:'',
+        phonenumber:''
+    })
+
+    console.log(login)
+
+    const onChange = e =>{
+
+        const { name, value } = e.target
+        setLogin({...login, [name]: value})
+    }
+
+    // const onSubmit = () => {
+
+
+    // }
+
     return(
         <div>
             <h1>Plantz?</h1>
-                
-                <h2>Login</h2>
-                <br/>
-                <label>Username
-                    <input
-                        name="username"
-                        type='text'
-                        placeholder='Enter Username'
-                        />
-                </label>
-                <br/>
-                <label>Password
-                    <input
-                        name="password"
-                        type="password"
-                        placeholder='Enter Password'
-
-                    />
-                </label>
-                <br/>
-                <label>Phone Number
-                    <input 
-                        type="tel" 
-                        id="phone" 
-                        name="phone"
-                        pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                        required/>    
-                </label>
+                <form>
                     
+                    <h2>Login</h2>
+                    <br/>
+                    <label>Username
+                        <input
+                            name="username"
+                            type='text'
+                            placeholder='Enter Username'
+                            onChange={onChange}/>
+                    </label>
+                    <br/>
+                    <label>Password
+                        <input
+                            name="password"
+                            type="password"
+                            placeholder='Enter Password'
+                            onChange={onChange}/>
+                    </label>
+                    <br/>
+                    <label>Phone Number
+                        <input 
+                            type="tel" 
+                            id="phone" 
+                            name="phone"
+                            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                            required
+                            placeholder='Enter Phone Number'
+                            onChange={onChange}/>    
+                    </label>
+                    <br/>
+                    <button>Login</button>
+                </form>        
                 
-
         </div>
     )
 }
