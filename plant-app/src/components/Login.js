e005cac21d70586e5dd8cc7cea6814492349fef9
Carlos Ramos <rmscrlos@gmail.com>
2021-01-27 00:02:34 -0500

    deleted old login.js and added new login.js to components folder

--- plant-app/src/components/Login.js
+++ plant-app/src/components/Login.js
@@ -1,28 +1,77 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
-import styled from 'styled-components';
+import styled from 'styled-components'
 
-export default function Login() {
-	return (
-		<div>
-			<h1>Plantz?</h1>
 
-			<h2>Login</h2>
-			<br />
-			<label>
-				Username
-				<input name="username" type="text" placeholder="Enter Username" />
-			</label>
-			<br />
-			<label>
-				Password
-				<input name="password" type="password" placeholder="Enter Password" />
-			</label>
-			<br />
-			<label>
-				Phone Number
-				<input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required />
-			</label>
-		</div>
-	);
+export default function Login (){
+
+    //State for login
+    const [login, setLogin] = useState({
+        username: '',
+        password:'',
+        phonenumber:''
+    })
+
+    
+    const [loginDone, setLoginDone ] = useState([])
+    
+    // Change function
+    const onChange = e =>{
+
+        const { name, value } = e.target
+        setLogin({...login, [name]: value})
+    }
+
+    //Submit function - 
+    const onSubmit = e => {
+        console.log('Login form submitted')
+        e.preventDefault();
+        
+        setLoginDone({...loginDone})
+        setLogin({
+            username: '',
+            password:'',
+            phonenumber:''
+        })
+    }
+
+    return(
+        <div>
+            <h1>Plantz?</h1>
+                <form onSubmit={onSubmit}>
+                    
+                    <h2>Login</h2>
+                    <br/>
+                    <label>Username
+                        <input
+                            name="username"
+                            type='text'
+                            placeholder='Enter Username'
+                            onChange={onChange}/>
+                    </label>
+                    <br/>
+                    <label>Password
+                        <input
+                            name="password"
+                            type="password"
+                            placeholder='Enter Password'
+                            onChange={onChange}/>
+                    </label>
+                    <br/>
+                    <label>Phone Number
+                        <input 
+                            type="tel" 
+                            id="phone" 
+                            name="phone"
+                            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                            required
+                            placeholder='Enter Phone Number'
+                            onChange={onChange}/>    
+                    </label>
+                    <br/>
+                    <button>Login</button>
+                </form>        
+                
+        </div>
+    )
 }
