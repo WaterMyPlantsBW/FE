c5f1303d1d20388adbb19223ee9e8ad1f4451892
Durrell Jules <dujules23@gmail.com>
2021-01-27 22:08:30 -0500

    changed button name to signup

--- plant-app/src/App.js
+++ plant-app/src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import Login from './components/Login';
+import SignUp from './components/SignUp';
 import Navbar from './components/Navbar';
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
-			<Login />
+			<SignUp />
 		</div>
 	);
 }
