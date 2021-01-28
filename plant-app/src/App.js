f8e0d15915487bcd0c19cd2b5e31f9f33c05b5ea
Carlos Ramos <rmscrlos@gmail.com>
2021-01-27 20:10:49 -0500

    fixed issue with login component being redeclared

--- plant-app/src/App.js
+++ plant-app/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import Login from './components/Login';
 import Navbar from './components/Navbar';
 
-import Login from './Login';
 function App() {
 	return (
 		<div className="App">
