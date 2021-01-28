8f2ffc9f962b64bc9712193edf63b0ded55c6db5
Carlos Ramos <rmscrlos@gmail.com>
2021-01-27 21:49:00 -0500

    set up react-router and fixed some of the styles of navbar items

--- plant-app/src/App.js
+++ plant-app/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Login from './components/Login';
 import Navbar from './components/Navbar';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
-			<Login />
+
+			<Switch>
+				<Route exact path="/login" component={Login} />
+				<Route exact path="/" component={null} />
+			</Switch>
 		</div>
 	);
 }
--- plant-app/src/components/Navbar.js
+++ plant-app/src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 //styled components
 const NavbarContainer = styled.div`
@@ -13,8 +14,11 @@ const NavbarContainer = styled.div`
 `;
 
 const Ul = styled.ul`
+	width: 25%;
 	display: flex;
 	list-style: none;
+	justify-content: space-around;
+	align-items: center;
 `;
 
 const NavItems = styled.li`
@@ -24,7 +28,7 @@ const NavItems = styled.li`
 
 const LoginBtn = styled(NavItems)`
 	background: rgba(0, 0, 0, 0.2);
-	margin: auto 0.5rem;
+
 	padding: 0.6rem 1rem;
 	border-radius: 10px;
 	margin-left: 1.5rem;
@@ -33,21 +37,19 @@ const LoginBtn = styled(NavItems)`
 		background: #a0d2da;
 		box-shadow: 0px 7px 28px -5px rgba(0, 0, 0, 0.52);
 		transition: all 0.2s ease;
-		margin-bottom: 0.2rem;
+		margin-bottom: 0.5rem;
 	}
 `;
 
 const SignUpBtn = styled(NavItems)`
 	background: #a0d2da;
-
-	margin: auto 0.5rem;
 	padding: 0.6rem 1rem;
 	border-radius: 10px;
 	box-shadow: 0px 7px 28px -5px rgba(0, 0, 0, 0.52);
 
 	&:hover {
-		transition: all 3s ease;
-		margin-bottom: 0.2rem;
+		transition: all 0.2s ease;
+		margin-bottom: 0.5rem;
 	}
 `;
 
@@ -56,9 +58,17 @@ function Navbar() {
 		<NavbarContainer>
 			<h1>Plantz?</h1>
 			<Ul>
-				<NavItems>Home</NavItems>
-				<LoginBtn>Log in</LoginBtn>
-				<SignUpBtn>Sign up</SignUpBtn>
+				<Link style={{ color: '#fff' }} to="/">
+					<NavItems>Home</NavItems>
+				</Link>
+
+				<Link style={{ color: '#fff' }} to="/login">
+					<LoginBtn>Log in</LoginBtn>
+				</Link>
+
+				<Link style={{ color: '#fff' }} to="/signup">
+					<SignUpBtn>Sign Up</SignUpBtn>
+				</Link>
 			</Ul>
 		</NavbarContainer>
 	);
