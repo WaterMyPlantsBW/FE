ca8f86399c74a4a360834d9d88a04f0fa0e9e99a
Durrell Jules <dujules23@gmail.com>
2021-01-29 21:49:04 -0500

    began plant-card

--- plant-app/src/App.js
+++ plant-app/src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import { Switch, Route } from 'react-router-dom';
-import SignUp from './components/SignUp';
+// import SignUp from './components/SignUp';
 
 function App() {
 	return (
 		<div className="App">
 			<Navbar />
-
-			<Switch>
+			
+			{/* <Switch>
 				<Route exact path="/signup" component={SignUp} />
 				<Route exact path="/" component={null} />
-			</Switch>
+			</Switch> */}
 		</div>
 	);
 }
---
+++ plant-app/src/components/PlantCard.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+export default function PlantCard (props){
+
+
+    return(
+        <div>
+            <h1>Plant Name</h1>
+                <div></div>
+            <div>
+                <div>Image Tag here</div>
+            </div>
+            <div>Species</div>
+            <div>Species</div>
+            
+        </div>
+        
+    )
+}
