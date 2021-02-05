866b988686104f9c5f1c9b918674f8bc884c6e21
Rafiullah Rahmati <rafiullahrahmati@Rafiullahs-MacBook-Pro.local>
2021-02-04 18:20:04 -0800

    connect home button to login

--- plant-app/src/components/Home.js
+++ plant-app/src/components/Home.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import "./home.css"
 
-function Home() {
+function Home(props) {
 	return (
 		<div className="Home-Container">
 			<section className = "Header-container">
 			<h1>Our Mission</h1>
             <p>With an easy to use interface for creating a plant watering schedule tailored to each individual plant, Water MyPlants will remind users when it's time to feed that foliage and quench your plants' thirst.</p>
-			<button type="button" onClick={()=>{}}>Let's Start</button>
+			<button type="button" onClick={()=>{props.history.push("plants")}}>Let's Start</button>
 			</section>
 
 		</div>
